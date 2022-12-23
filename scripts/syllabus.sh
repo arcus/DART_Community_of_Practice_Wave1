0be85c7ba394d1d0d7a944de40d497a5f0942e39
DRELLICHE <drelliche@chop.edu>
2022-12-23 13:17:24 -0500

    update table

--- scripts/syllabus.sh
+++ scripts/syllabus.sh
@@ -4,7 +4,7 @@
 # First we create the .md file
 #############################
 
-COLUMNS="Week Topic Activity WarmUp Discussion Extra"
+COLUMNS="Topic Activity WarmUp Discussion Extra"
 
 # Create a .md file with the column headings specified above
 
@@ -17,17 +17,17 @@ do
 done
 
 # Headings are not automated but rather hanve specified widths because otherwise some columns are way to narrow.
-echo "| Week | <div style=\"width:120px\">Topic</div> | <div style=\"width:200px\">Activity</div> | <div style=\"width:200px\">Warm-Up</div> | <div style=\"width:400px\">Discussion Topic</div> | <div style=\"width:200px\">Extra</div> |" > syllabus.md
+echo "| <div style=\"width:120px\">Topic</div> | <div style=\"width:200px\">Activity</div> | <div style=\"width:200px\">Warm-Up</div> | <div style=\"width:400px\">Discussion Topic</div> | <div style=\"width:200px\">Extra</div> |" > syllabus.md
 echo $BREAK >> syllabus.md
 
 
 ## TODO: markdown doesn't like lists inside of tables, covert to pure(er) HTML to make them render nicely?
 for FOLDER in Prompts/*
 do
-  ROW='|'
+  ROW='| **Week '
   program_week=$(grep "program_week:" $FOLDER/theme.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]')
-  ROW+=$program_week
-  ROW+='|'
+  ROW+=$"$(echo -e "${program_week}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+  ROW+=":**"
   section=" `grep "section:" $FOLDER/theme.md | sed "s/^[^ ]* //" | sed "s/^[ ]* //" | tr -dc '[:print:]'` "
   ROW+=$section
   ROW+=' Part '
