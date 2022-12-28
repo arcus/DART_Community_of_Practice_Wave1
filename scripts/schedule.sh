a3d4dc00ccfb8d5c7c213e6c2f015f9f40d8a2ba
DRELLICHE <drelliche@chop.edu>
2022-12-28 10:57:59 -0500

    clean up schedule visually

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,23 +1,23 @@
 ### Make mini-schedule
 
-echo "Community of Practice Schedule" > schedule.md
+echo "# Community of Practice Schedule" > schedule.md
 week=1
 
 for FOLDER in Prompts/*
 do
     echo >> schedule.md
-    
-    echo "[`basename $FOLDER`]($FOLDER)" >> schedule.md ### prints the folder name without the whole path
-    
+    topic=`basename $FOLDER` 
+    echo "## [${topic//_/ }]($FOLDER)" >> schedule.md ### prints the folder name without the whole path
+    echo "## [${topic//_/ }]($FOLDER)"
 
     for SUBFOLDER in $FOLDER/*    
     do
         #echo $week
-        subtopic=`basename $SUBFOLDER` >> schedule.md
-        echo "- [$subtopic]($SUBFOLDER)" >> schedule.md
+        subtopic=`basename $SUBFOLDER`
+        echo "- [${subtopic//_/ }]($SUBFOLDER)" >> schedule.md
         week=$(($week+1))
     done
     
     echo >> schedule.md
-    
+
 done
